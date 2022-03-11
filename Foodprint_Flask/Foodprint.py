0ff18547cfd1afe3279394aeff3955661681ecaa
PixelsDE <carina.schoppe2912@gmail.com>
2022-03-11 19:12:07 +0100

    carina sophies commit 2

--- Foodprint_Flask/Foodprint.py
+++ Foodprint_Flask/Foodprint.py
@@ -31,7 +31,7 @@ def create():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    from Foodprint_Flask.models import User
+    from models import User
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
